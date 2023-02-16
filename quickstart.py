36e3351e0f0084f1aacf8c46fc69a27587b370ec
MrWardKKHS <60589645+MrWardKKHS@users.noreply.github.com>
2023-02-16 14:57:59 +1300

    got a window open

---
+++ quickstart.py
@@ -0,0 +1,13 @@
+import arcade
+
+class MyGame(arcade.Window):
+    def __init__(self) -> None:
+        super().__init__(800, 800, "My awesome game")
+        arcade.set_background_color((50, 50, 200))
+
+    def on_draw(self):
+        self.clear()
+        arcade.draw_circle_filled(300, 300, 30, arcade.color.AUROMETALSAURUS)
+
+game = MyGame()
+arcade.run()
